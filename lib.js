d644090437c488a76dd8d051516e9e33f099b1e4
jseb <joel@tutero.com>
2013-08-17 21:57:16 +0200

    observable

--- lib.js
+++ lib.js
@@ -1,25 +1,48 @@
 /*global window: false*/
-(function (window) {
+(function (window, event) {
     'use strict';
     var myModule = {},
-        options = {
+        defaultOptions = {
             'limit': 10
         },
+        settings = defaultOptions,
+        eventListeners = {},
         callCount = 0;
     function incrementCallCount() {
         callCount = callCount + 1;
     }
-    myModule.set = function (newOptions) {
-        options = newOptions;
-    }
+    myModule.on = function (type, listener) {
+        if (typeof eventListeners[type] === 'undefined') {
+            eventListeners[type] = [];
+        }
+        eventListeners[type].push(listener);
+    };
+    myModule.fire = function (eventName) {
+        var i, len, listeners;
+        event = { 'type': eventName };
+        if (!event.target) {
+            event.target = this;
+        }
+        if (eventListeners[event.type] instanceof Array) {
+            listeners = eventListeners[event.type];
+            for (i = 0, len = listeners.length; i < len; i += 1) {
+                listeners[i].call(this, event);
+            }
+        }
+    };
+    myModule.set = function (options) {
+        settings = options;
+    };
     myModule.getStatistics = function () {
         return 'Number of calls to echo method: ' + callCount;
     };
     myModule.echo = function (input) {
-        if (options.limit > callCount) {
+        if (settings.limit <= callCount) {
+            this.fire('limit');
+        } else {
             incrementCallCount();
             return input;
         }
     };
     window.myModule = myModule;
-}(window));
+}(window, window.event));
