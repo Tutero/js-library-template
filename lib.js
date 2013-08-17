6553f27a7c9abe4aeaaa257c2af95dd6299509ec
jseb <joel@tutero.com>
2013-08-17 17:46:34 +0200

    added options, 3rd point

--- lib.js
+++ lib.js
@@ -2,16 +2,24 @@
 (function (window) {
     'use strict';
     var myModule = {},
-        callCount = 0; // can't be read or set from outside
-    function incrementCallCount() { // can't be called from outside
+        options = {
+            'limit': 10
+        },
+        callCount = 0;
+    function incrementCallCount() {
         callCount = callCount + 1;
     }
-    myModule.echo = function (input) {
-        incrementCallCount();
-        return input;
-    };
+    myModule.set = function (newOptions) {
+        options = newOptions;
+    }
     myModule.getStatistics = function () {
-        return 'Number of calls to echo method: ' + this.getCallCount();
+        return 'Number of calls to echo method: ' + callCount;
+    };
+    myModule.echo = function (input) {
+        if (options.limit > callCount) {
+            incrementCallCount();
+            return input;
+        }
     };
     window.myModule = myModule;
 }(window));
